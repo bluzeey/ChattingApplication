ec9f079e9ebdd6e8c4a05c9d0b53a5a379bc47a6
Sahil Maheshwari <sahilm1711@gmail.com>
2021-09-13 11:51:21 +0530

    Adding the files of react

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ClientState, StreamChat} from 'stream-chat';
+import {ChannelList, Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+import {ChannelContainer,ChannelListContainer} from './components'
+
+import './App.css'
+const apiKey='77fntt8jptw8'
+const client= StreamChat.getInstance(apiKey)
+function App() {
+  return (
+     <div className="app__wrapper">
+       <Chat client={client} theme="team light">
+         <ChannelListContainer/>
+         <ChannelContainer/>
+       </Chat>
+     </div>
+  );
+}
+
+export default App;
